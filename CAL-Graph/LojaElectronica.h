e1b1dc41dd361a4e8ddd1c80dc3b8c40fcacdc3c
jorgemiguelreis <jorgemiguelreis@users.noreply.github.com>
2013-03-24 15:58:58 +0000

    apenas documentação inicial de LojaElectronica

--- CAL-Graph/LojaElectronica.h
+++ CAL-Graph/LojaElectronica.h
@@ -1,8 +1,12 @@
 /*
- * LojaElectronica.h
- *
- *  Created on: 23 de Mar de 2013
- *      Author: Miao
+ * 	FICHEIRO: LojaElectronica.h
+ *	PROJECTO: Projecto CAL - Loja de Comercio Electronico (T3)
+ *	DESCRICAO: Definicao da classe LojaElectronica
+ *  TURMA / GRUPO: Turma 2MIEIC2 / Grupo D
+ *  AUTORES:
+ *  - Jorge Miguel Reis
+ *  - Miao Sun
+ *  - Vitor Castro
  */
 
 #ifndef LOJAELECTRONICA_H_
